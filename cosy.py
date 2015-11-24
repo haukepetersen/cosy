0b24bd2a592a9333c5af5175e948b91b1367fc66
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-11-24 17:54:35 +0100

    now getting correct numbers

--- cosy.py
+++ cosy.py
@@ -20,7 +20,7 @@ import sys
 from os import path
 import argparse
 import re
-# import subprocess
+import subprocess
 
 
 # class Anasize():
@@ -84,8 +84,7 @@ import re
 #         print("   text    data     bss     dec     hex filename")
 #         print("%7i %7i %7i %7i %7x %s" % (text, data, bss, dec, dec, self.elf))
 
-#         sizedump = subprocess.check_output((self.prefix + 'size', self.elf))
-#         print '\n' + sizedump
+
 
 
 
@@ -94,7 +93,8 @@ import re
 if __name__ == "__main__":
     # Define some command line args
     p = argparse.ArgumentParser()
-    p.add_argument("file", default="test.elf", nargs="?", help="ELF file to analyze")
+    p.add_argument("file", default="test.map", nargs="?", help="MAP file")
+    p.add_argument("elf", default="test.elf", nargs="?", help="ELF file")
     p.add_argument("-p", default="", help="Toolchain prefix, e.g. arm-none-eabi-")
     args = p.parse_args()
 
@@ -102,21 +102,92 @@ if __name__ == "__main__":
     if not path.isfile(args.file):
         sys.exit("Error: ELF file '" + args.file + "' does not exist")
 
+
     f = open(args.file, 'r')
 
     l = 0
+
+    mem_t = dict()
+    mem_t['target'] = 0
+    mem_t['sum'] = 0
+    mem_t['fill'] = 0
+
+    mem_b = dict()
+    mem_b['target'] = 0
+    mem_b['sum'] = 0
+    mem_b['fill'] = 0
+
+    mem_d = dict()
+    mem_d['target'] = 0
+    mem_d['sum'] = 0
+    mem_d['fill'] = 0
+
+    state = ''
+
+    mem = {}
+    cur_symbol = ''
+
     for line in f:
 
-        if re.match("^ \.vectors", line):
-            print "####" + line,
-
-        m = re.match(' *\.text\.([\._-a-zA-Z0-9]+)', line)
+        m = re.match("^\.text +0x[0-9a-f]+ +0x([0-9a-f]+)", line)
         if m:
-            print line,
-            l += 1
+            mem_t['target'] = int(m.group(1), 16)
+            # state = 'text'
+            mem = mem_t
+            continue
+
+        m = re.match("^\.bss +0x[0-9a-f]+ +0x([0-9a-f]+)", line)
+        if m:
+            mem_b['target'] = int(m.group(1), 16)
+            # state = 'bss'
+            mem = mem_b
+            continue
+
+        m = re.match("^\.(relocate|data) +0x[0-9a-f]+ +0x([0-9a-f]+)", line)
+        if m:
+            mem_d['target'] += int(m.group(2), 16)
+            # state = 'data'
+            mem = mem_d
+            continue
+
+        m = re.match("^\.stack +0x[0-9a-f]+ +0x([0-9a-f]+)", line)
+        if m:
+            mem_b['target'] += int(m.group(1), 16)
+            mem = mem_b
+            continue
+
+        m = re.match("^\.[a-z0-9]+", line)
+        if m:
+            # state = ''
+            mem = {}
+            continue
+
+
+        if mem:
+            m = re.match("^ *\*fill\* +0x[0-9a-z]+ +0x([0-9a-z])+", line)
+            if m:
+                mem['fill'] += int(m.group(1), 16)
+
+            m = re.match(" (\.[-_\.A-Za-z0-9]+)", line)
+            if m:
+                cur_symbol = m.group(1)
+
+            m = re.match(".+0x[0-9a-f]+ +0x([0-9a-f]+)", line)
+            if m:
+                mem['sum'] += int(m.group(1), 16)
+
+
 
     print(args.file + " contains %i lines" % l)
 
     # Analyze ELF file
     # ana = Anasize(args.file, args.p)
     # ana.parseFile()
+
+    print("text - target: %i, sum: %i, fill: %i" % (mem_t['target'], mem_t['sum'], mem_t['fill']))
+    print("data - target: %i, sum: %i, fill: %i" % (mem_d['target'], mem_d['sum'], mem_d['fill']))
+    print("bss  - target: %i, sum: %i, fill: %i" % (mem_b['target'], mem_b['sum'], mem_b['fill']))
+    print ""
+
+    # DEGBUG: output size results
+    print subprocess.check_output((args.p + 'size', args.elf)),
