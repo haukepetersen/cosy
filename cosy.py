5f306f81c0365c36a02e80e3928386feeec028ed
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-11-25 23:32:51 +0100

    asdf

--- cosy.py
+++ cosy.py
@@ -43,131 +43,126 @@ if __name__ == "__main__":
         args.elf])
 
     for line in dump.splitlines():
-        m = re.match("([0-9a-f]+) [a-zA-Z] ([_a-zA-Z0-9]+)[ \t]+.+/(RIOT/.+):(\d+)$", line)
+        m = re.match("([0-9a-f]+) ([tbdTDB]) ([_a-zA-Z0-9]+)[ \t]+.+/(RIOT/.+):(\d+)$", line)
         if m:
-            symbols[m.group(2)] = {
+            symbols[m.group(3)] = {
+                'type': m.group(2).lower(),
                 'addr': m.group(1),
-                'path': m.group(3),
-                'line': int(m.group(4)),
+                'line': int(m.group(5)),
+                'size': 0,
+                'path': m.group(4),
             }
 
 
 
-    f = open(args.file, 'r')
 
-    l = 0
 
-    root = {'name': 'RIOT', 't': 0, 'd': 0, 'b': 0, 'children': []}
 
-    mem_t = dict()
-    mem_t['target'] = 0
-    mem_t['sum'] = 0
-    mem_t['fill'] = 0
+    targets = {'t': 0, 'd': 0, 'b': 0}
 
-    mem_b = dict()
-    mem_b['target'] = 0
-    mem_b['sum'] = 0
-    mem_b['fill'] = 0
-
-    mem_d = dict()
-    mem_d['target'] = 0
-    mem_d['sum'] = 0
-    mem_d['fill'] = 0
-
-    state = ''
-
-    cur_symbol = ''
+    cur_type = ''
+    cur_sect = ''
+    cur_addr = 0
     cur_size = 0
-    cur_path = ''
-    sym_names = []
+    cur_obj = ''
+    cur_arc = ''
+    cur_alias = []
 
-    mem = {}
-
+    f = open(args.file, 'r')
     for line in f:
 
         m = re.match("^\.text +0x[0-9a-f]+ +0x([0-9a-f]+)", line)
         if m:
-            mem = mem_t
-            mem['target'] = int(m.group(1), 16)
+            targets['t'] += int(m.group(1), 16)
+            cur_type = 't'
             continue
 
         m = re.match("^\.bss +0x[0-9a-f]+ +0x([0-9a-f]+)", line)
         if m:
-            mem = mem_b
-            mem['target'] += int(m.group(1), 16)
+            targets['b'] += int(m.group(1), 16)
+            cur_type = 'b'
             continue
 
         m = re.match("^\.stack +0x[0-9a-f]+ +0x([0-9a-f]+)", line)
         if m:
-            mem = mem_b
-            mem['target'] += int(m.group(1), 16)
+            targets['b'] += int(m.group(1), 16)
+            cur_type = 'b'
             continue
 
         m = re.match("^\.(relocate|data) +0x[0-9a-f]+ +0x([0-9a-f]+)", line)
         if m:
-            mem = mem_d
-            mem['target'] = int(m.group(2), 16)
+            targets['d'] += int(m.group(2), 16)
+            cur_type = 'd'
             continue
-
 
         m = re.match("^\.[a-z0-9]+", line)
         if m:
-            mem = {}
+            cur_type = ''
             continue
 
 
-        if mem:
-            m = re.match("^ *\*fill\* +0x[0-9a-z]+ +0x([0-9a-z])+", line)
+        if cur_type:
+            # fill bytes?
+            m = re.match("^ *\*fill\* +0x[0-9a-f]+ +0x([0-9a-f])+", line)
             if m:
-                mem['fill'] += int(m.group(1), 16)
-                mem['sum'] += int(m.group(1), 16)
+                if "fill_" + cur_type in symbols:
+                    symbols["fill_" + cur_type]['size'] += int(m.group(1), 16)
+                else:
+                    symbols["fill_" + cur_type] = {'size': int(m.group(1), 16), 'type': cur_type}
+                continue
 
+            # start of a new symbol
             m = re.match(" (\.[-_\.A-Za-z0-9]+)", line)
             if m:
 
-                if sym_names:
+                # process last symbol
+                if cur_sect:
+                    if len(cur_alias) == 1:
+                        name = cur_alias[0]
+                        print "NAME " + name
+                    else:
+                        print "there are " + str(len(cur_alias)) + " aliases for " + cur_sect
 
-
-                    # print "FOUND:" + cur_symbol + " (" + str(cur_size) + ") " + cur_path
-                    # print sym_names
-                    sym = re.match(".*\.([-_a-zA-Z0-9]+)", cur_symbol)
+                    sym = re.match(".*\.([-_a-zA-Z0-9]+)", cur_sect)
                     if sym:
                         if sym.group(1) not in symbols:
                             print sym.group(1) + " is new!"
-                        else:
-                            print "--- found " + sym.group(1) + " size: " + str(cur_size)
-                            print "----      " + symbols[sym.group(1)]['path']
+                        # else:
+                            # print "--- found " + sym.group(1) + " size: " + str(cur_size)
+                            # print "----      " + symbols[sym.group(1)]['path']
                     else:
-                        print "NO MATCH: " + cur_symbol
+                        print("ERROR ERROR")
 
-                    for sym in sym_names:
-                        # print sym
-                        if sym in symbols:
-                            print "+++ found " + sym
+                    # for sym in sym_names:
+                    #     # print sym
+                    #     if sym in symbols:
+                    #         print "+++ found " + sym
+                    cur_alias = []
 
-                    sym_names = []
+                cur_sect = m.group(1)
 
-                cur_symbol = m.group(1)
-
-
-
-            m = re.match(".+0x[0-9a-f]+ +0x([0-9a-f]+) (/.+)$", line)
+            m = re.match(".+0x([0-9a-f]+) +0x([0-9a-f]+) (/.+)$", line)
             if m:
-                cur_size = int(m.group(1), 16)
-                mem['sum'] += int(m.group(1), 16)
-                cur_path = m.group(2)
+                cur_addr = int(m.group(1), 16)
+                cur_size = int(m.group(2), 16)
+                cur_arc = m.group(3)
                 continue
 
             m = re.match(" +0x[0-9a-f]+ +([-_a-zA-Z0-9]+)$", line)
             if m:
-                sym_names.append(m.group(1))
+                cur_alias.append(m.group(1))
 
+    res = {'t': 0, 'd': 0, 'b': 0, 'sum': 0}
 
+    for key in symbols:
+        res[symbols[key]['type']] += symbols[key]['size']
+        res['sum'] += symbols[key]['size']
 
-    print("text - target: %i, sum: %i, fill: %i" % (mem_t['target'], mem_t['sum'], mem_t['fill']))
-    print("data - target: %i, sum: %i, fill: %i" % (mem_d['target'], mem_d['sum'], mem_d['fill']))
-    print("bss  - target: %i, sum: %i, fill: %i" % (mem_b['target'], mem_b['sum'], mem_b['fill']))
-    print ""
+    print("targets:")
+    print("text: %i, data: %i, bss: %i" % (targets['t'], targets['d'], targets['b']))
+    print("restuls:")
+    print("text: %i, data: %i, bss: %i, sum: %i" % (res['t'], res['d'], res['b'], res['sum']))
+
 
     # DEGBUG: output size results
     print subprocess.check_output((args.p + 'size', args.elf)),
